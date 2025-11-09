aa8f1990848de208d59d22f088e0ff0c80e1a55e
Satoshi OKUI <okuisatoshi@mac.com>
2025-11-09 18:05:48 +0900

    修正

--- misc/cfg.sh
+++ misc/cfg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 type=${1:-pdf}
-opt -passes=dot-cfg >-
+opt -passes=dot-cfg >/dev/null
 sed -i -e '2d' -e 's/, align 4//g' -e 's/style=filled,//' .main.dot
 dot -T $type .main.dot
 rm -f .main.dot
